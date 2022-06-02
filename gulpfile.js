8f987725b1c3d025d242303b6a1ffc3fa71c4fad
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-06-03 01:40:42 +0200

    Use dart-sass as sass compiler engine
    Change due to node-sass being deprecated
    and throwing errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, watch, series, parallel } = require('gulp');
 
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
+sass.compiler = require('sass');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
@@ -18,7 +19,7 @@ const files = {
 function scssTask(){    
     return src(files.scssPath)
         .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass([])) // compile SCSS to CSS
+        .pipe(sass().on('error', sass.logError)) // compile SCSS to CSS
         .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
         .pipe(dest('dist')
