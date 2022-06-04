92de808bb53ebcbdd772ced555fa74f4d5cbd6d2
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-06-04 04:42:51 +0200

    Add browsersync for convenience

--- gulpfile.js
+++ gulpfile.js
@@ -8,48 +8,60 @@ const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-var replace = require('gulp-replace');
+const replace = require('gulp-replace');
+const browserSync = require('browser-sync').create();
 
 // File paths
-const files = { 
+const files = {
     scssPath: 'src/scss/**/*.scss',
     jsPath: 'src/js/**/*.js'
 };
 
-function scssTask(){    
+function scssTask() {
     return src(files.scssPath)
         .pipe(sourcemaps.init()) // initialize sourcemaps first
         .pipe(sass().on('error', sass.logError)) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
         .pipe(dest('dist')
-    ); // put final CSS in dist folder
+        ); // put final CSS in dist folder
 }
 
-function jsTask(){
+function jsTask() {
     return src([
         files.jsPath
-        ])
+    ])
         .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(dest('dist')
-    );
+        );
 }
 
 var cbString = new Date().getTime();
-function cacheBustTask(){
+function cacheBustTask() {
     return src(['index.html'])
         .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
         .pipe(dest('.'));
 }
 
-function watchTask(){
-    watch([files.scssPath, files.jsPath], 
-        parallel(scssTask, jsTask));    
+function watchTask() {
+    watch([files.scssPath, files.jsPath],
+        parallel(scssTask, jsTask));
 }
 
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    watch([files.scssPath, files.jsPath, "./index.html"],
+        parallel(scssTask, jsTask)).on('change', browserSync.reload);
+};
+
 exports.default = series(
-    parallel(scssTask, jsTask), 
+    parallel(scssTask, jsTask),
     cacheBustTask,
-    watchTask
+    serve,
 );
