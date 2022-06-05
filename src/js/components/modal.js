db34bb37d415be2a701ad8f5b2f319f12a69536a
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-06-05 15:16:49 +0200

    Add onClickOutside event to modal

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -4,14 +4,25 @@
 const makeModal = (el) => {
   const trigger = document.querySelector(`[data-target="#${el.id}"]`);
   const closeBttn = el.querySelector(".modal__close-btn");
+  const content = el.querySelector(".modal__content");
+
+  /**
+   * @param {Event} event
+   */
+  const clickOutside = (event) => {
+    if (!content.contains(event.target)) hide();
+  };
 
   const show = () => {
     el.style.display = "block";
+    document.addEventListener("click", clickOutside);
     el.focus();
   };
 
   const hide = () => {
     el.style.display = "none";
+    document.removeEventListener("click", clickOutside);
+    trigger.focus();
   };
 
   const toggle = () => {
@@ -22,13 +33,13 @@ const makeModal = (el) => {
     }
   };
 
-  trigger.addEventListener("click", () => {
+  trigger.addEventListener("click", (e) => {
+    e.stopPropagation();
     show();
   });
 
   closeBttn.addEventListener("click", () => {
     hide();
-    trigger.focus();
   });
 
   return {
